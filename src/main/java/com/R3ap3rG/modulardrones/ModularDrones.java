46371829a12eaac8c4d0cdb2b9b0f7e57dea2683
R3ap3rG <owen_fielder@hotmail.com>
2015-01-25 14:48:00 +0000

    Added WorldGen for ores

--- src/main/java/com/R3ap3rG/modulardrones/ModularDrones.java
+++ src/main/java/com/R3ap3rG/modulardrones/ModularDrones.java
@@ -1,10 +1,13 @@
 package com.R3ap3rG.modulardrones;
 
-import com.R3ap3rG.modulardrones.block.DevBlock;
+import com.R3ap3rG.modulardrones.block.ModBlocks;
 import com.R3ap3rG.modulardrones.client.model.ModelDroneBase;
 import com.R3ap3rG.modulardrones.client.renderer.entity.RenderDroneBase;
 import com.R3ap3rG.modulardrones.entity.EntityDroneBase;
 import com.R3ap3rG.modulardrones.handler.ConfigHandler;
+import com.R3ap3rG.modulardrones.handler.Recipes;
+import com.R3ap3rG.modulardrones.handler.WorldGenMDOres;
+import com.R3ap3rG.modulardrones.item.ModItems;
 import com.R3ap3rG.modulardrones.reference.Reference;
 import com.R3ap3rG.modulardrones.util.LogHelper;
 import cpw.mods.fml.client.registry.RenderingRegistry;
@@ -15,8 +18,6 @@ import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.registry.EntityRegistry;
 import cpw.mods.fml.common.registry.GameRegistry;
-import net.minecraft.block.Block;
-import net.minecraft.block.material.Material;
 
 @Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION)
 public class ModularDrones {
@@ -24,7 +25,7 @@ public class ModularDrones {
     @Mod.Instance("ModularDrones")
     public static ModularDrones instance;
 
-    public static Block devBlock;
+    WorldGenMDOres eventOreWorldGen = new WorldGenMDOres();
 
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event){
@@ -41,14 +42,17 @@ public class ModularDrones {
         RenderingRegistry.registerEntityRenderingHandler(EntityDroneBase.class, new RenderDroneBase(new ModelDroneBase(),0.3F));
         EntityRegistry.registerGlobalEntityID(EntityDroneBase.class, "DroneBase", EntityRegistry.findGlobalUniqueEntityId(), 0x7AE8FF, 0x47FFE2);
 
-        devBlock = new DevBlock(Material.glass);
-        GameRegistry.registerBlock(new DevBlock(Material.glass),"devBlock");
+        ModItems.init();
+        ModBlocks.init();
+        Recipes.init();
 
         LogHelper.info("Initialisation Complete!");
     }
 
     @Mod.EventHandler
     public void postInit(FMLPostInitializationEvent event){
+
+        GameRegistry.registerWorldGenerator(eventOreWorldGen, 0);
 
         LogHelper.info("PostInitialisation Complete!");
     }
