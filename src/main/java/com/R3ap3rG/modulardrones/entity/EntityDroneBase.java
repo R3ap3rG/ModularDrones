ae2b6c14c04d81268f8f55dc74d9825b606aafad
R3ap3rG <owen_fielder@hotmail.com>
2015-01-25 14:49:39 +0000

    Changes to EntityDroneBase

--- src/main/java/com/R3ap3rG/modulardrones/entity/EntityDroneBase.java
+++ src/main/java/com/R3ap3rG/modulardrones/entity/EntityDroneBase.java
@@ -4,6 +4,7 @@ import net.minecraft.entity.EntityAgeable;
 import net.minecraft.entity.EnumCreatureAttribute;
 import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.ai.EntityAIHurtByTarget;
+import net.minecraft.entity.ai.EntityAIWander;
 import net.minecraft.entity.ai.EntityAIWatchClosest;
 import net.minecraft.entity.passive.EntityTameable;
 import net.minecraft.entity.player.EntityPlayer;
@@ -14,21 +15,29 @@ public class EntityDroneBase extends EntityTameable {
     public EntityAgeable createChild(EntityAgeable p_90011_1_) {
         return null;
     }
+    //todo
     public EntityDroneBase(World world)
     {
         super(world);
-        this.getNavigator().setBreakDoors(true);
-        this.tasks.addTask(8, new EntityAIWatchClosest(this, EntityPlayer.class, 8.0F));
+        this.tasks.addTask(2, new EntityAIWatchClosest(this, EntityPlayer.class, 8.0F));
+        this.tasks.addTask(7, new EntityAIWander(this,1.0D));
         this.targetTasks.addTask(1, new EntityAIHurtByTarget(this, true));
         this.setSize(0.6F, 1.8F);
-        this.isTamed();
     }
 
+
+    public boolean allowLeashing()
+    {
+        return false;
+    }
+    //todo
     protected void applyEntityAttributes()
     {
         super.applyEntityAttributes();
         this.getEntityAttribute(SharedMonsterAttributes.followRange).setBaseValue(40.0D);
-        this.getEntityAttribute(SharedMonsterAttributes.movementSpeed).setBaseValue(0.5D);
+        this.getEntityAttribute(SharedMonsterAttributes.movementSpeed).setBaseValue(0.3D);
+        this.getEntityAttribute(SharedMonsterAttributes.maxHealth).setBaseValue(20.0D);
+        this.getEntityAttribute(SharedMonsterAttributes.knockbackResistance).setBaseValue(0.8D);
     }
 
     @Override
@@ -38,6 +47,12 @@ public class EntityDroneBase extends EntityTameable {
         this.getDataWatcher().addObject(29, (byte) 0);
         this.getDataWatcher().addObject(30, (byte) 0);
         this.getDataWatcher().addObject(31, (byte) 0);
+    }
+
+    //todo
+    public boolean interact(EntityPlayer player)
+    {
+        return true;
     }
 
     protected boolean isAIEnabled()
